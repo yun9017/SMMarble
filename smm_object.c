3e3f6ea54a6152870ae72bf60bd4136e5550eb1c
yun9017 <yun9017ie@naver.com>
2025-11-06 16:44:51 +0900

    base

---
+++ smm_object.c
@@ -0,0 +1,39 @@
+//
+//  smm_node.c
+//  SMMarble
+//
+//  Created by Juyeop Kim on 2023/11/05.
+//
+
+#include "smm_common.h"
+#include "smm_object.h"
+#include <string.h>
+
+#define MAX_NODETYPE    7
+#define MAX_GRADE       9
+
+
+
+//object generation
+void smmObj_genNode(void)
+{
+    
+}
+
+
+
+//member retrieving
+
+
+
+//element to string
+char* smmObj_getNodeName(smmNode_e type)
+{
+    return smmNodeName[type];
+}
+
+char* smmObj_getGradeName(smmGrade_e grade)
+{
+    return smmGradeName[grade];
+}
+
---
+++ smm_object.h
@@ -0,0 +1,46 @@
+//
+//  smm_object.h
+//  SMMarble object
+//
+//  Created by Juyeop Kim on 2023/11/05.
+//
+
+#ifndef smm_object_h
+#define smm_object_h
+
+/* node type :
+    lecture,
+    restaurant,
+    laboratory,
+    home,
+    experiment,
+    foodChance,
+    festival
+*/
+
+
+/* grade :
+    A+,
+    A0,
+    A-,
+    B+,
+    B0,
+    B-,
+    C+,
+    C0,
+    C-
+*/s
+
+
+
+//object generation
+int smmObj_genNode(void);
+
+//member retrieving
+
+
+//element to string
+
+
+
+#endif /* smm_object_h */
